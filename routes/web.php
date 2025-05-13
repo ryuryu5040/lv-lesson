f8f49fddf955ae0dc5ef286930a69262540aa03b
ryuryu5040 <reirisssarasara@gmail.com>
2025-05-13 13:13:22 +0000

    lv-04 commit

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Route::get('/', function () {
 
 use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
 
-
 Route::get('/', [PostController::class, 'index']);   
+
+Route::post('/posts', [PostController::class, 'store']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+
